9ee07274ba8174527ef0733a69ea8aac394fece8
Sulabasana <piotr.frydryszak@gmail.com>
2020-02-06 20:40:28 +0100

    Read the documentation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (){
     var fields = [
              { name: "id", type: "string" },
              { name: "status", map: "state", type: "string" },
@@ -12,48 +12,152 @@ $(document).ready(function () {
     //Test purpose    
     var valuePair = ['1','new', 'Create Bot', 'Future', 'high', '3']
 
+    let inputs = $('input')
+    let arr = []
+    // console.log(inputs)
+    let but = $('#generate')
+    
+    but.click(function(event){
+        inputs.each(function(){
+            if($(this).val() === ""){
+                // alert("Empty Fields!!");
+                console.log('Click')
+                event.preventDefault()
+                return false
+            }else{
+                let value = $(this).val()
+                arr.push(value)
+                // console.log(arr)
+                // console.log(arr.length)
+                if(arr.length == 4){
+                    let small1 = arr[0].toLowerCase()
+                    arr[0]=small1
+                    ///NAPRAWIĆ !!!!!!
+                    // let capital1 = arr[1].capitalize()
+                    // arr[1]=capital1
+                    // let capital2 = arr[2].capitalize()
+                    // arr[2]=capital2
+                    let small2 =arr[3].toLowerCase()
+                    arr[3]=small2
+                }
+                if(arr.length >= 4){
+                    addToObj()
+                }
+                
+                // console.log(arr)
+                
+                console.log(source.localData)
+                
+                display()
+                event.preventDefault()
+            }
+         
+        })
+           
+        
+        console.log(arr)
+        console.log(source.localData)
+        // checkform(this)
+        event.preventDefault()
+        
+        
+        arr =[]
+        inputs.each(function(){
+            $(this).val('')
+        })
+
+    });
 
     //Set color according to severity
-    if(valuePair[4] =="high"){
+    // if(valuePair[4] =="high"){
+    //     hex = '#000'
+    // }else if(valuePair[4] =="medium"){
+    //     hex ="#ea34"
+    // }else{
+    //     hex ="#0ee73e"
+    // }
+    if(arr[3] =="high"){
         hex = '#000'
-    }else if(valuePair[4] =="medium"){
+    }else if(arr[3] =="medium"){
         hex ="#ea34"
     }else{
         hex ="#0ee73e"
     }
     //obj that will be added to Kanban
-    var obj = {
-        'id': valuePair[0],
-        'state': valuePair[1],
-        'activity': valuePair[2],
-        'tags': valuePair[3],
-        'hex' : hex,
-        'resourceID': valuePair[5],
+    // var obj = {
+    //     'id': valuePair[0],
+    //     'state': valuePair[1],
+    //     'activity': valuePair[2],
+    //     'tags': valuePair[3],
+    //     'hex' : hex,
+    //     'resourceID': valuePair[5],
     
+    // }
+    function addToObj(){
+        obj = {
+            'id': Math.floor(Math.random() * 10).toString(),
+            'state': arr[0],
+            'activity': arr[1],
+            'tags': arr[2],
+            'hex' : hex,
+            'resourceID': Math.floor(Math.random() * 10).toString()
+        
+        }
+        source.localData.push(obj)
     }
+        
+    
+
+    
+            // console.log("I am here")
+        // $("form[name='registration']").validate({
+        //     // Specify validation rules
+        //     rules: {
+        //     // The key name on the left side is the name attribute of an input field. Validation rules are defined on the right side
+        //     status: "required",
+        //     text: "required",
+        //     tags: 'required',
+        //     color: 'required',
+
+        //     },
+        //     // Specify validation error messages
+        //     messages: {
+        //     status: "Select between: new, work, done",
+        //     text: "What are you working on",
+        //     tags: "Tag related to the task",
+        //     color: "Low, Medium, High"
+        //     },
+        //     // Make sure the form is submitted to the destination defined in the "action" attribute of the form when valid
+        //     submitHandler: function(form) {
+        //     form.submit();
+        //     }
+        // });
+        // function checkform(form) {
+        //     // get all the inputs within the submitted form
+            
+        //     for (var i = 0; i < inputs.length; i++) {
+        //         console.log('Yej'+ i)
+        //         // only validate the inputs that have the required attribute
+        //         if(inputs[i].hasAttribute("required")){
+        //             if(inputs[i].value == ""){
+        //                 // found an empty field that is required
+                        
+        //                 alert("Please fill all required fields");
+                        
+        //                 return false;
+        //             }else{
+        //                 but.attr('disabled', false)
+        //             }
+        //         }
+        //     }
+        //     return true;
+        // }
+    
+    
 
     //Adding new items to Kanban
     //let displayer = $('#displayer')
-    let inputs = $('input')
-    let arr = []
-    // console.log(inputs)
-    let but = $('#generate')
-    but.click(function(event){
-        inputs.each(function(){
-            let value = $(this).val()
-            arr.push(value)
-            
-        })
-        let small = arr[2].toLowerCase()
-        arr[2]=small
-        console.log(small)
-        console.log(arr)
-        source.localData.push(obj)
-        console.log(source.localData)
-        display()
-             
-        event.preventDefault()
-    });
+   
     //Data for Kanban
     var source =
      {
